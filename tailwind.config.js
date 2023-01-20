13988e73149a27b990b55cca043918d7dd7322aa
Alejandro Bay <alejandrobay23@gmail.com>
2023-01-20 15:49:53 -0500

    NEw data

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        // Pacifico: ['Pacifico', ...defaultTheme.fontFamily.pacifico],
+        pacifico: ['Pacifico'],
       },
     },
   },
